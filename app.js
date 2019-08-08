f3b1b8b4469c01ba7eb041d196683fe7b0b9943c
MaxxBravo <maxxbr.iee@gmail.com>
2019-08-08 17:29:28 -0300

    commit #2

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
 // app.js
+console.log('Iniciado');
 
 var request = require('request');
 var express = require('express');
+var bodyParser = require('body-parser');
 
-console.log('Iniciado');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/views', express.static(__dirname + '/views'));
 
+var CONTACTS_INFO;
 
 request.get({
-    headers: {'content-type': 'application/json'},
     url: `https://s3.amazonaws.com/technical-challenge/v3/contacts.json`
 }, (err, response, body) => {
     if (!err) {
-        // console.log();
-        console.log(JSON.parse(body));
-        // console.log(body);
-        // console.log('algo');
-        
+        CONTACTS_INFO = JSON.parse(body)
+        console.log(CONTACTS_INFO[0]);
     } else {
         
     }
 });
+app.get('/', (req, res) => {
+    
+    res.render('pages/contacts')
+});
+
+function getFilteredFavorites(flag){
+    let filtered_contacts = CONTACTS_INFO.filter(contact => contact.isFavorite==flag);
+    
+    filtered_contacts.map((contact) => {
+        console.log(contact.name);
+        
+    })
+    return filtered_contacts;
+}
+
+
+app.get('/showFavorites', (req, res) => {
+    res.send(getFilteredFavorites(true));
+});
+
+app.get('/showNotFavorites', (req, res) => {
+    res.send(getFilteredFavorites(false));
+});
+
+app.put('/showFavorites', (req, res) => {
+    res.send(getFilteredFavorites(true));
+});
+
+app.put('/showNotFavorites', (req, res) => {
+    res.send(getFilteredFavorites(false));
+});
+
+const server = app.listen(5000, () => {
+    const PORT = server.address().port;
+    const ADDRESS = server.address().address || 'localhost';
+    console.log(`App listening on URL: https://${ADDRESS}:${PORT}`);
+    
+})
---
+++ views/custom.js
@@ -0,0 +1,32 @@
+let $FAVS = $('#favContacts');
+let $OTHERS = $('#otherContacts');
+
+function showProfilePictures($zone, data){    
+    $zone.append(data.map(contact => 
+        $(`<div class="fader"><img src="${contact.smallImageURL}" style="max-height: 75px"></div>`)
+    )); 
+
+    $('.fader', $zone).on('click', function(){
+        $(this).slideUp();
+    });
+}
+
+
+$(document).ready(() => {   
+    $FAVS.css('background', 'grey');
+    $OTHERS.css('background', 'blue');
+    $.ajax({
+        url: '/showFavorites',
+        method: 'PUT',
+        success: (data) => {
+            showProfilePictures($FAVS, data);
+        }
+    })
+    $.ajax({
+        url: '/showNotFavorites',
+        method: 'PUT',
+        success: (data) => {
+            showProfilePictures($OTHERS, data);
+        }
+    })
+});
